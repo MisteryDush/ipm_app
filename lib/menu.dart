9f5096c7ff2ab1c76dfa26efe36cace3dabd501a
MisteryDush <i.arifdjanov@yandex.ru>
2023-03-05 16:07:53 +0800

    Edit design

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,19 +1,109 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'api/user.dart';
 
+const Color textColorGold = Color.fromRGBO(209, 142, 48, 1);
+const Color backgroundColorIndigo = Color.fromRGBO(49, 0, 94, 1);
+
 class Menu extends StatelessWidget {
   User user = User.instance;
+  var totalFormat = NumberFormat("###,###.0#", "en_US");
+  var percentageFormat = NumberFormat("###,###.0#", "en_US");
+  var differenceFormat = NumberFormat("###,###.0####", "en_US");
 
   @override
   Widget build(BuildContext context) {
-    print('Welcome ${user.getName} !');
-    print('Total weight: ${user.getTotalWeight}');
-    print('Total value: \$${user.getTotalValue}');
-    print('You have these commodities:');
-    user.getCommodities;
-    print(user.getInjections);
-    return const Text("Hi!");
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: backgroundColorIndigo,
+          toolbarHeight: 70,
+          title: Image.asset(
+            'assets/images/logo_and_name.png',
+            height: 50,
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.zero,
+          child: SizedBox(
+            height: 1000,
+            child: Column(
+              children: [
+                Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Text(
+                      'Welcome\n${user.getName}!',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        color: textColorGold,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    )),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 30),
+                        child: Image.asset(
+                          'assets/images/line.png',
+                          width: 120,
+                          fit: BoxFit.fill,
+                        )),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 30),
+                        child: Image.asset(
+                          'assets/images/star.png',
+                          width: 37,
+                          fit: BoxFit.fill,
+                        )),
+                    Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 30),
+                        child: Image.asset(
+                          'assets/images/line.png',
+                          width: 120,
+                          fit: BoxFit.fill,
+                        ))
+                  ],
+                ),
+                Padding(
+                    padding: EdgeInsets.zero,
+                    child: Column(children: [
+                      Text(
+                        totalFormat.format(user.getTotalValue),
+                        style: TextStyle(
+                          fontSize: 30,
+                        ),
+                      ),
+                      Text(
+                        percentageFormat.format(user.getValueDifference),
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: getColor(user.getValueDifference),
+                        ),
+                      ),
+                      Text(
+                        differenceFormat.format(user.getValueDifferencePercentage),
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: getColor(user.getValueDifferencePercentage)
+                        ),
+                      )
+                    ])),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
+  Color getColor(double value) {
+    return value < 0 ? Colors.red : Colors.green;
+  }
 }
