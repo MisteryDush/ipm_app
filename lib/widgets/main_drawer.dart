82730f83109270a5355cf43e8bd02ae116eca9d7
mjoshua218 <joshuajames.mitchell@my.jcu.edu.au>
2023-03-07 18:06:46 +0800

    Updated the drawer to put the 'settings' on the bottom left corner

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -7,90 +7,93 @@ class MainDrawer extends StatelessWidget{
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: backgroundColorIndigo,
-      child: ListView(
-        padding: EdgeInsets.zero,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          ListTile(
-            leading: Icon(
-              Icons.home,
-              size: 30,
-              color: textColorGold,
+          Expanded(
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    size: 30,
+                    color: textColorGold,
+                  ),
+                  title: const Text(
+                    'Home',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    if(ModalRoute.of(context)?.settings.name != '/menu') {
+                      Navigator.popAndPushNamed(context, '/menu');
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.area_chart_outlined,
+                    size: 30,
+                    color: textColorGold,
+                  ),
+                  title: const Text(
+                    'Historical Vault Valuations',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.attach_money,
+                    size: 30,
+                    color: textColorGold,
+                  ),
+                  title: const Text(
+                    'Vault/MGT % Cost Charges',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.price_change_outlined,
+                    size: 30,
+                    color: textColorGold,
+                  ),
+                  title: const Text(
+                    'Injections Page',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(
+                    Icons.file_download,
+                    size: 30,
+                    color: textColorGold,
+                  ),
+                  title: const Text(
+                    'Download Reports',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
             ),
-            title: const Text(
-              'Home',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              if(ModalRoute.of(context)?.settings.name != '/menu') {
-                Navigator.popAndPushNamed(context, '/menu');
-              }
-            },
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.area_chart_outlined,
-              size: 30,
-              color: textColorGold,
-            ),
-            title: const Text(
-              'Historical Vault Valuations',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.attach_money,
-              size: 30,
-              color: textColorGold,
-            ),
-            title: const Text(
-              'Vault/MGT % Cost Charges',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.price_change_outlined,
-              size: 30,
-              color: textColorGold,
-            ),
-            title: const Text(
-              'Injections Page',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
-          ),
-          ListTile(
-            leading: Icon(
-              Icons.file_download,
-              size: 30,
-              color: textColorGold,
-            ),
-            title: const Text(
-              'Download Reports',
-              style: TextStyle(color: Colors.white),
-            ),
-            onTap: () {
-              Navigator.pop(context);
-            },
           ),
           ListTile(
             leading: Icon(
               Icons.settings,
               size: 30,
               color: textColorGold,
-            ),
-            title: const Text(
-              'Settings',
-              style: TextStyle(color: Colors.white),
             ),
             onTap: () {
               if(ModalRoute.of(context)?.settings.name != '/settings') {
@@ -102,5 +105,4 @@ class MainDrawer extends StatelessWidget{
       ),
     );
   }
-
 }
