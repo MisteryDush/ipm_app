debab366beb18d3634bb98e530bea9c3af603bb9
mjoshua218 <joshuajames.mitchell@my.jcu.edu.au>
2023-03-08 00:42:18 +0800

    Fixed the dropdownbutton for the settings page

--- lib/settings.dart
+++ lib/settings.dart
@@ -13,76 +13,90 @@ class SettingsPage extends StatefulWidget {
 
 
 class _SettingsPageState extends State<SettingsPage> {
-
   GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
 
-  String? _selectedCurrency = 'USD';
-  String? _selectedWeight = 'Toz';
+  String? _selectedCurrency;
+  String? _selectedWeight;
 
   List<String> _currencies = ['USD', 'EUR', 'GBP', 'MYR', 'SGD', 'AUD'];
   List<String> _weights = ['Toz', 'Kilo', 'Grams'];
 
+  @override
+  void initState() {
+    super.initState();
+    _loadPreferences();
+  }
+
+  void _loadPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _selectedCurrency = prefs.getString('Currency') ?? 'USD';
+      _selectedWeight = prefs.getString('Weight') ?? 'Toz';
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     User user = User.instance;
 
     return Scaffold(
-        appBar: MainAppBar(_scaffoldKey),
-        body: Scaffold(
-          key: _scaffoldKey,
-          drawer: MainDrawer(),
-          body: Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  'Currency',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
-                DropdownButton<String>(
-                  value: _selectedCurrency,
-                  onChanged: (String? newValue) {
-                    setState(() async {
-                      _selectedCurrency = newValue;
-                      final prefs = await SharedPreferences.getInstance();
-                      prefs.setString('Currency', '$newValue');
-                      user.setChosenCurrency = newValue!;
-                    });
-                  },
-                  items:
-                      _currencies.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-                SizedBox(height: 16.0),
-                Text(
-                  'Weight',
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                ),
-                DropdownButton<String>(
-                  value: _selectedWeight,
-                  onChanged: (String? newValue) {
-                    setState(() async {
-                      final prefs = await SharedPreferences.getInstance();
-                      _selectedWeight = newValue;
-                      prefs.setString('Weight', '$newValue');
-                    });
-                  },
-                  items: _weights.map<DropdownMenuItem<String>>((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                ),
-              ],
-            ),
+      appBar: MainAppBar(_scaffoldKey),
+      body: Scaffold(
+        key: _scaffoldKey,
+        drawer: MainDrawer(),
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                'Currency',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              DropdownButton<String>(
+                value: _selectedCurrency,
+                onChanged: (String? newValue) async {
+                  final prefs = await SharedPreferences.getInstance();
+                  setState(() {
+                    _selectedCurrency = newValue;
+                    prefs.setString('Currency', '$newValue');
+                    user.setChosenCurrency = _selectedCurrency!;
+                  });
+                },
+                items:
+                _currencies.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                'Weight',
+                style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+              DropdownButton<String>(
+                value: _selectedWeight,
+                onChanged: (String? newValue) async {
+                  final prefs = await SharedPreferences.getInstance();
+                  setState(() {
+                    _selectedWeight = newValue;
+                    prefs.setString('Weight', '$newValue');
+                    user.setChosenCurrency = _selectedCurrency!;
+                  });
+                },
+                items: _weights.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
