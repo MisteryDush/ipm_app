24f7121960c27289ab05b27cba6266b643e41e05
MisteryDush <i.arifdjanov@yandex.ru>
2023-03-12 20:10:17 +0800

    Update: Create class for metal charges

---
+++ lib/api/metal_charge.dart
@@ -0,0 +1,31 @@
+class MetalCharge {
+  String _name = '';
+  double _costPerDay = 0.0;
+  double _chargePercent = 0.0;
+  double _weight = 0.0;
+  double _value = 0.0;
+
+
+  MetalCharge(this._name, this._costPerDay, this._chargePercent, this._weight, this._value);
+
+  get getName{
+    return _name;
+  }
+
+  get getCostPerDay{
+    return _costPerDay;
+  }
+
+  get getChargePercent{
+    return _chargePercent;
+  }
+
+  get getWeight{
+    return _weight;
+  }
+
+  get getValue{
+    return _value;
+  }
+
+}
