8115b5db66c2f218c1dd4a7ace55de8c8e627eab
MisteryDush <i.arifdjanov@yandex.ru>
2023-03-22 22:25:31 +0800

    Update: Add line chart

--- lib/historical_vault_page.dart
+++ lib/historical_vault_page.dart
@@ -4,6 +4,9 @@ import 'package:ipm_app/widgets/main_app_bar.dart';
 import 'package:ipm_app/widgets/main_drawer.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
+import 'api/metal_historic_performance.dart';
+import 'api/user.dart';
+
 class HistoricalVaultPage extends StatefulWidget {
   @override
   _HistoricalVaultPageState createState() => _HistoricalVaultPageState();
@@ -12,7 +15,11 @@ class HistoricalVaultPage extends StatefulWidget {
 class _HistoricalVaultPageState extends State<HistoricalVaultPage> {
   GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey();
 
+  User user = User.instance;
+
   Widget build(BuildContext context) {
+    MetalHistoricPerformance metal = user.allHist;
+    MetalHistoricPerformance spotPrices = user.getSpotPrices;
     var height = 0.0;
 
     if (MediaQuery.of(context).orientation == Orientation.landscape) {
@@ -29,95 +36,80 @@ class _HistoricalVaultPageState extends State<HistoricalVaultPage> {
         body: SizedBox(
             child: SingleChildScrollView(
                 child: Padding(
-              padding: EdgeInsets.zero,
-              child: SizedBox(
-                height: height,
-                child: Column(
+          padding: EdgeInsets.zero,
+          child: SizedBox(
+            height: height,
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25),
+                  child: Text(
+                    'Historical Vault Valuations',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: textColorGold,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Padding(
-                      padding: EdgeInsets.symmetric(vertical: 25),
-                      child: Text(
-                        'Historical Vault Valuations',
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          color: textColorGold,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 30,
-                        ),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 30),
+                      child: Image.asset(
+                        'assets/images/logo_divide.png',
+                        width: 320,
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 30),
-                          child: Image.asset(
-                            'assets/images/logo_divide.png',
-                            width: 320,
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ],
-                    ),
-                    SfCartesianChart(
-                      primaryXAxis: CategoryAxis(),
-                      series: <ChartSeries>[
-                        LineSeries<ChartData, String>(
-                            dataSource: [
-                              ChartData('Jan', 35),
-                              ChartData('Feb', 23),
-                              ChartData('Jun', 25),
-                              ChartData('Mar', 35),
-                              ChartData('Apr', 23),
-                              ChartData('May', 25),
-                              ChartData('Sep', 35),
-                              ChartData('Oct', 23),
-                              ChartData('Nov', 25),
-                              ChartData('Dec', 35),
-                              ChartData('Jul', 23),
-                              ChartData('Aug', 25),
-                              ChartData('Jan1', 35),
-                              ChartData('Feb1', 23),
-                              ChartData('Jun1', 25),
-                              ChartData('Mar1', 35),
-                              ChartData('Apr1', 23),
-                              ChartData('May1', 25),
-                              ChartData('Sep1', 35),
-                              ChartData('Oct1', 23),
-                              ChartData('Nov1', 25),
-                              ChartData('Dec1', 35),
-                              ChartData('Jul1', 23),
-                              ChartData('Aug1', 25),
-                              ChartData('Jan2', 35),
-                              ChartData('Feb2', 23),
-                              ChartData('Jun2', 25),
-                              ChartData('Mar2', 35),
-                              ChartData('Apr2', 23),
-                              ChartData('May2', 25),
-                              ChartData('Sep2', 35),
-                              ChartData('Oct2', 23),
-                              ChartData('Nov2', 25),
-                              ChartData('Dec2', 35),
-                              ChartData('Jul2', 23),
-                              ChartData('Aug2', 25),
-                            ],
-                            xValueMapper: (ChartData data, _) => data.x,
-                            yValueMapper: (ChartData data, _) => data.y)
-                      ],
-                    )
                   ],
                 ),
-              ),
-            ))),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 25),
+                  child: Text(
+                    'Charts Values are being displayed in ${user.getChosenCurrency}',
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+                SfCartesianChart(
+                  primaryXAxis: CategoryAxis(),
+                  axes: [CategoryAxis(name: 'yAxis', opposedPosition: true)],
+                  legend: Legend(
+                      isVisible: true,
+                      position: LegendPosition.bottom,
+                      overflowMode: LegendItemOverflowMode.wrap),
+                  series: <ChartSeries>[
+                    LineSeries<ChartData, String>(
+                        name:
+                            '${metal.getName.toCapitalized()} Vault Stock Valuation',
+                        dataSource: metal.getChartData,
+                        xValueMapper: (ChartData data, _) => data.label,
+                        yValueMapper: (ChartData data, _) => (data.data *
+                            user.currencyRates[user.getChosenCurrency]!)),
+                    LineSeries<ChartData, String>(
+                        name:
+                            '${spotPrices.getName.toCapitalized()} Vault Stock Valuation (Right-Hand axis)',
+                        dashArray: <double>[10,10],
+                        dataSource: spotPrices.getChartData,
+                        xValueMapper: (ChartData data, _) => data.label,
+                        yValueMapper: (ChartData data, _) =>
+                            data.data *
+                            user.currencyRates[user.getChosenCurrency]!,
+                        yAxisName: 'yAxis')
+                  ],
+                )
+              ],
+            ),
+          ),
+        ))),
       ),
     );
   }
-}
-
-class ChartData {
-  ChartData(this.x, this.y);
-
-  final String x;
-  final double? y;
 }
